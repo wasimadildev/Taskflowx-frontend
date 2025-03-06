c78f84a42ffd7c1eb1b433568bd14ed5d6f608ea
Wasim Adil <waseemadilcode@gmail.com>
2025-03-06 12:15:12 +0500

    Fixed TypeScript errors and updated priority levels

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -22,7 +22,7 @@ const badgeVariants = cva(
         [TaskStatusEnum.IN_REVIEW]: "bg-purple-100 text-purple-500",
         [TaskStatusEnum.DONE]: "bg-green-100 text-green-600",
         [TaskPriorityEnum.HIGH]: "bg-orange-100 text-orange-600",
-        [TaskPriorityEnum.URGENT]: "bg-red-100 text-red-600",
+        
         [TaskPriorityEnum.MEDIUM]: "bg-yellow-100 text-yellow-600",
         [TaskPriorityEnum.LOW]: "bg-gray-100 text-gray-600",
       },
